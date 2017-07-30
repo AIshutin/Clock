3c7db2e848e8099a7bb07d5f89a649c0a11a3699
AIshutin <hazmozavr@gmail.com>
2017-07-30 17:14:37 +0300

    Basic version
    It works. Simply.

---
+++ MyClock.bat
@@ -0,0 +1,15 @@
+@echo off
+rem AI`s Software
+:begin
+rem 45 * 60 = 2700
+ping -n 10 127.0.0.1 >nul
+ring.mp3
+ping -n 10 127.0.0.1 >nul
+music.wpl
+rem 5 * 60 = 300 
+ping -n 10 127.0.0.1 >nul
+taskkill /im wwahost.exe >nul
+taskkill /im wmplayer.exe >nul
+taskkill /im vlc.exe >nul
+cls
+goto begin
---
+++ README.md
@@ -0,0 +1,8 @@
+AI`s Software
+
+Simple clock. It uses a ring.mp3 as ringtone and music.wpl as soundtrack to 5 minutes break.
+45 minutes you work.
+1 minutes you terminate your actions
+5 minutes you do something without computer.
+
+You also can change ring.mp3 and music.wpl files
