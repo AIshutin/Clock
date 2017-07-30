c42ab709549832ad0adde61eb989aeb80d721942
AIshutin <hazmozavr@gmail.com>
2017-07-30 17:19:22 +0300

    Small fix
    Small fix for time duration

--- MyClock.bat
+++ MyClock.bat
@@ -2,12 +2,12 @@
 rem AI`s Software
 :begin
 rem 45 * 60 = 2700
-ping -n 10 127.0.0.1 >nul
+ping -n 2700 127.0.0.1 >nul
 ring.mp3
-ping -n 10 127.0.0.1 >nul
+ping -n 60 127.0.0.1 >nul
 music.wpl
 rem 5 * 60 = 300 
-ping -n 10 127.0.0.1 >nul
+ping -n 300 127.0.0.1 >nul
 taskkill /im wwahost.exe >nul
 taskkill /im wmplayer.exe >nul
 taskkill /im vlc.exe >nul
